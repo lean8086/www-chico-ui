73694738813ee10a9786f13af096e5f7bbdf03ff
Natan Santolo <natan.santolo@gmail.com>
2011-07-07 17:48:50 -0300

    Add robots.txt, new dinamic navigation, some new howtos and demos

--- app.js
+++ app.js
@@ -4,14 +4,14 @@
  */
 
 var express = require('express'),
-    events = require('events'),
-    sys = require('sys'),
-    fs = require('fs'),
-//    gzippo = require('gzippo'),
-    CustomBuild = require('./services/builder/custom_build').CustomBuild,
-    Encode64 = require('./services/builder/encode64').Encode64,
-    meta = require('./models/meta').meta;
-    undefined;
+		events = require('events'),
+		sys = require('sys'),
+		fs = require('fs'),
+//		gzippo = require('gzippo'),
+		CustomBuild = require('./services/builder/custom_build').CustomBuild,
+		Encode64 = require('./services/builder/encode64').Encode64,
+		meta = require('./models/meta').meta;
+		undefined;
 
 
 var app = module.exports = express.createServer();
@@ -24,22 +24,23 @@ var app = module.exports = express.createServer();
  * app configuration.
  */
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
 // gzip compression
-//  app.use(gzippo.staticGzip(__dirname + '/public'));
+//	app.use(gzippo.staticGzip(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+	app.use(express.errorHandler()); 
 });
 
 /**
@@ -47,25 +48,25 @@ app.configure('production', function(){
 
 // build lastest version
 app.get( '/lastest/:type/:min?', function( req, res ) {
-       
-    var type = req.params.type;
-    var min = req.params.min;
-    
-    var packages = [{
-        "name": "chico",
-        "input": "../chico/src/"+type+"/",
-        "type": type,
-        "min": (min) ? true : false,
-        "virtual": true // Don't write the files
-    }];
-    
-    var custom = new CustomBuild( packages );
-        custom.avoidCompress();
-        custom.on( "processed" , function( data ) {
-            res.header('Content-Type', (type==="js") ? "text/javascript" : "text/css" );
-            res.send( data );
-        });
-    
+ 			
+		var type = req.params.type;
+		var min = req.params.min;
+		
+		var packages = [{
+				"name": "chico",
+				"input": "../chico/src/"+type+"/",
+				"type": type,
+				"min": (min) ? true : false,
+				"virtual": true // Don't write the files
+		}];
+		
+		var custom = new CustomBuild( packages );
+				custom.avoidCompress();
+				custom.on( "processed" , function( data ) {
+						res.header('Content-Type', (type3==="js") ? "text/javascript" : "text/css" );
+						res.send( data );
+				});
+		
 });
 */
 
@@ -74,98 +75,98 @@ app.get( '/lastest/:type/:min?', function( req, res ) {
  */
 // get 
 app.get( '/download', function( req, res ) {
-    
-  res.render( 'download', meta );
-  
+		
+	res.render( 'download', meta );
+	
 });
 
 // post 
 app.post('/download', function( req, res ){
 
-//    console.log( req.body );
+//		console.log( req.body );
 
-    var components = ( typeof req.body.class === "object" ) ? // If collection 
-                        req.body.class.join(",") : 
-                        req.body.class,
-        abstracts = ( typeof req.body.abstract === "object" ) ? // If collection 
-                        req.body.abstract.join(",") : 
-                        req.body.abstract,
-        utils = ( typeof req.body.util === "object" ) ? // If collection 
-                        req.body.util.join(",") : 
-                        req.body.util,
-        flavor = req.body.flavor,
-        use_mesh = req.body.mesh,
-        embed = req.body.embed;
-        env = req.body.env;
+		var components = ( typeof req.body.class === "object" ) ? // If collection 
+												req.body.class.join(",") : 
+												req.body.class,
+				abstracts = ( typeof req.body.abstract === "object" ) ? // If collection 
+												req.body.abstract.join(",") : 
+												req.body.abstract,
+				utils = ( typeof req.body.util === "object" ) ? // If collection 
+												req.body.util.join(",") : 
+												req.body.util,
+				flavor = req.body.flavor,
+				use_mesh = req.body.mesh,
+				embed = req.body.embed;
+				env = req.body.env;
 
-    if ( !abstracts || !utils || !components ) {
-        res.send(); // Avoid process without components
-        return;
-    }
+		if ( !abstracts || !utils || !components ) {
+				res.send(); // Avoid process without components
+				return;
+		}
 
-    // JavaScripts Pack
-    var js = function() {
-        return {
-            "name": "chico",
-            "input": "../chico/src/js/",
-            "components": abstracts.toLowerCase() + "," + utils.toLowerCase() + "," + components.toLowerCase(),
-            "type": "js"
-        }
-    };
+		// JavaScripts Pack
+		var js = function() {
+				return {
+						"name": "chico",
+						"input": "../chico/src/js/",
+						"components": abstracts.toLowerCase() + "," + utils.toLowerCase() + "," + components.toLowerCase(),
+						"type": "js"
+				}
+		};
 
-    // Stylesheets Pack
-    var css = function() {
-        return {
-            "name": "chico",
-            "input": "../chico/src/" + flavor + "/css/",
-            "components": components + ( ( use_mesh ) ? ",mesh" : "" ),
-            "embed": ( embed ) ? true : false,
-            "type": "css"
-        }
-    };
-    
-    // Mesh Pack
-    var mesh = function() {
-        return {
-            "name": "mesh",
-            "input": "../chico/src/css/",
-            "components": "mesh",
-            "embed": ( embed ) ? true : false,
-            "type": "css"
-        }
-    };
+		// Stylesheets Pack
+		var css = function() {
+				return {
+						"name": "chico",
+						"input": "../chico/src/" + flavor + "/css/",
+						"components": components + ( ( use_mesh ) ? ",mesh" : "" ),
+						"embed": ( embed ) ? true : false,
+						"type": "css"
+				}
+		};
+		
+		// Mesh Pack
+		var mesh = function() {
+				return {
+						"name": "mesh",
+						"input": "../chico/src/css/",
+						"components": "mesh",
+						"embed": ( embed ) ? true : false,
+						"type": "css"
+				}
+		};
 
-    // Pack the thing
-    var packages = [];
-    // for Production
-    if ( env.toString().indexOf("p") > -1 ) {
-        // JS
-        var p_js = js();
-            p_js.min = true;
-            packages.push(p_js);
-        // CSS
-        var p_css = css();
-            p_css.min = true;
-            packages.push(p_css);
-    }
-    
-    // for Dev
-    if ( env.toString().indexOf("d") > -1 ) {
-        // JS
-        var d_js = js();
-            packages.push( d_js );
-        // CSS
-        var d_css = css();
-            packages.push( d_css );
-    }
+		// Pack the thing
+		var packages = [];
+		// for Production
+		if ( env.toString().indexOf("p") > -1 ) {
+				// JS
+				var p_js = js();
+						p_js.min = true;
+						packages.push(p_js);
+				// CSS
+				var p_css = css();
+						p_css.min = true;
+						packages.push(p_css);
+		}
+		
+		// for Dev
+		if ( env.toString().indexOf("d") > -1 ) {
+				// JS
+				var d_js = js();
+						packages.push( d_js );
+				// CSS
+				var d_css = css();
+						packages.push( d_css );
+		}
 
-    console.log(packages);
-    
-    var custom = new CustomBuild( packages );
-        custom.on("done", function( uri ) {
-            //res.send( uri );
-            res.redirect( uri );
-        });
+		console.log(packages);
+		
+		var custom = new CustomBuild( packages );
+				custom.on("done", function( uri ) {
+						//res.send( uri );
+						res.redirect( uri );
+				});
 
 });
 
@@ -174,15 +175,16 @@ app.post('/download', function( req, res ){
  */
 // get
 app.get('/snippets', function(req, res){
-  res.render('snippets', meta );
+	res.render('snippets', meta );
 });
 
 /**
- * Support.
+ * Discussion.
  */
 // get
-app.get('/support', function(req, res){
-  res.render('support', meta );
+app.get('/discussion', function(req, res){
+	meta.title = "Discussion on Chico UI";
+	res.render('discussion', meta );
 });
 
 /**
@@ -190,7 +192,9 @@ app.get('/support', function(req, res){
  */
 // get
 app.get('/getting-started', function(req, res){
-  res.render('getting-started', meta );
+	// new title
+	meta.title = "Getting started with Chico UI";
+	res.render('getting-started', meta );
 });
 
 /**
@@ -199,38 +203,64 @@ app.get('/getting-started', function(req, res){
 // get
 app.get('/how-to/:label?', function(req, res){
 
-  // get label
-  var label = req.params.label;
+	// get label
+	var label = req.params.label;
 
-  // no label, go to getting started  
-  if (label == undefined) {
-    res.render('getting-started', meta );
+	// no label, go to getting started	
+	if (label == undefined) {
+		res.render('getting-started', meta );
 	return;
-  }
+	}
 
-  // re define title
-  var title = label.split("-").join(" ");
-    
-  // new title
-  meta.title = title;
-  
-  // render
-  res.render('how-to/'+label, meta );
-  
+	// re define title
+	var title = label.split("-").join(" ");
+		
+	// new title
+	var _meta = Object.create(meta);
+		_meta.title = title;
+	
+	// render
+	res.render('how-to/'+label, _meta );
+	
 });
+/**
+ * Demos
+ */
+// get
+app.get('/demos/:label?', function(req, res){
 
+	// get label
+	var label = req.params.label;
+
+	// no label, go to getting started	
+	if (label == undefined) {
+		res.render('getting-started', meta );
+	return;
+	}
+
+	// re define title
+	var title = label.split("-").join(" ");
+
+	// new title
+	var _meta = Object.create(meta);
+		_meta.title = title + " demo.";
+
+	// render
+	res.render('demos/'+label, _meta );
+	
+});
 
 /**
  * Index.
  */
 // get
 app.get('/', function(req, res){
-  res.render('index', meta );
+	res.render('index', meta );
 });
 
 /**
  * app start
- */
+ */ 
 app.listen(8080);
 
 /**
--- models/meta.js
+++ models/meta.js
@@ -1,13 +1,16 @@
 
 var meta = {
+	
 	"title": "Chico UI, the toolbox for the Web.",
+	
 	"navigation": {
+	
 		"top": [
 			{"label": "Home", "href": "/"},
 			{"label": "Download", "href": "/download"},
 			{"label": "Getting Started", "href": "/getting-started"},
 			{"label": "API", "href": "/api/index.html"},
-			{"label": "Support", "href": "/support"}
+			{"label": "Discussion", "href": "/discussion"},
 		],
 
 		"bottom": [
@@ -15,8 +18,8 @@ var meta = {
 			{"label": "Current release", "href": "/download"},
 			{"label": "Past releases", "href": "http://download.chico-ui.com.ar/"},
 			{"label": "Source code", "href": "https://github.com/mercadolibre/chico/"},
-			{"label": "Support", "href": "/support/faq"},
-			{"label": "FAQ", "href": "/support/faq"},
+			{"label": "Discussion", "href": "/discussion"},
+			{"label": "FAQ", "href": "/faq"},
 			{"label": "Issue tracker", "href": "https://github.com/visionmedia/jade/issues"},
 			{"label": "Mailing list", "href": "https://groups.google.com/group/chico-ui"},
 			{"label": "Get in touch", "href": "/"},
@@ -26,12 +29,37 @@ var meta = {
 		]
 
 	},
+	
 	"howtos": [
 		{"label": "Mesh your layout in 5 min", "href": "/how-to/mesh-your-layout-in-5-min"},
 		{"label": "Change the content of a modal", "href": "/how-to/change-the-content-of-modal-window"},
-		{"label": "Validate two fields like one", "href": "/how-to/validate-two-fields-like-one"}
+		{"label": "Validate two fields like one", "href": "/how-to/validate-two-fields-like-one"},
+		{"label": "Use callbacks functions", "href": "/how-to/use-callback-functions"}
+	],
+	
+	"demos": [
+/*		{"label": "Cache", "href": "/demos/cache"},*/
+		{"label": "Calendar", "href": "/demos/calendar"},
+		{"label": "Carousel", "href": "/demos/carousel"},
+		{"label": "Dropdown", "href": "/demos/dropdown"},
+/*		{"label": "Factory", "href": "/demos/factory"},*/
+		{"label": "Form", "href": "/demos/form"},
+/*		{"label": "Eraser", "href": "/demos/eraser"},*/
+		{"label": "Expando", "href": "/demos/expando"},
+/*		{"label": "Extend", "href": "/demos/extend"},
+		{"label": "Keyboard", "href": "/demos/keyboard"},*/
+		{"label": "Layer", "href": "/demos/layer"},
+/*		{"label": "List", "href": "/demos/list"},*/
+		{"label": "Menu", "href": "/demos/menu"},
+		{"label": "Modal", "href": "/demos/modal"},
+/*		{"label": "onImagesLoads", "href": "/demos/onImagesLoads"},*/
+		{"label": "Positioner", "href": "/demos/positioner"},
+/*		{"label": "Preload", "href": "/demos/preload"},*/
+		{"label": "TabNavigator", "href": "/demos/tabNavigator"},
+		{"label": "Tooltip", "href": "/demos/tooltip"},
+		{"label": "Watcher", "href": "/demos/watcher"},
+		{"label": "Zoom", "href": "/demos/zoom"}
 	]
-
 }
 
 /* -----[ Exports ]----- */
