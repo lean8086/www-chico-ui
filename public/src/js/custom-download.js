576f8d0a22f6debf2bf4fccba03ada9bd716f212
Natan Santolo <natan.santolo@gmail.com>
2011-08-29 18:14:49 -0300

    Release 0.7.5

--- public/src/js/custom-download.js
+++ public/src/js/custom-download.js
@@ -8,40 +8,42 @@ $.getJSON("/data/inheritanceMap.js", function(data){
 
     map = data;
     custom.populate();
+    /*
+    $("#abstracts, #utils, #components").each(function(i,e){
+    	$(e).parent()
+	    	.hover(custom.select.add, custom.select.remove);
+    });*/
     
-    $("#components input, #utils input").change(function(e){
-        
-        var c = map[$(this).val()];
-        
-        custom.action = (!$(this).attr("checked")) ? custom.unCheckIt : custom.checkIt ;
-        
-        // Check the map
-        custom.checkMap(c);
-    
-    });
 });
-var help = {
-    
-    _list: [],
-    
-    init: function() {
-    
-        var a = "<p><a href=\"#\">Turn off help layers.</a></p>";
-        
-    },
-    
-    add: function(o) {},
-    
-    on: function() {},
-    
-    off: function() {}
-    
-}
+
 var custom = {
-    
-    components: [],
-    
-    utils: [],
+
+	select: {
+		
+		action: function(e) {
+			var isChecked = $(this).parent().find("input:checked");
+			if (isChecked) {
+				$(this).parent().find("input").removeAttr("checked");
+			} else {
+				$(this).parent().find("input").attr("checked","checked");
+			}
+			
+		},
+		
+		tpl: $("<a class=\"all\">toggle select</a>"),
+		
+		add: function(w){
+			custom.select.tpl
+				.bind("click", custom.select.action)
+				.prependTo(this);
+		},
+		
+		remove: function(){
+			custom.select.tpl
+				.unbind("click", custom.select.action)
+				.detach(this);
+		}
+	},
     
     populate: function() {
         
@@ -54,13 +56,10 @@ var custom = {
                 switch ( type ) {
                     case "class": 
                         return "#components";
-                    case "interface":
-                        return "#components > li:last-child";
                     case "abstract":
                         return "#abstracts";
                     case "util":
                         return "#utils";
-                    
                 }
                                 
             })(); 
@@ -72,76 +71,7 @@ var custom = {
             
             map[c]["dom"] = $("<li class=\""+className+"\"><label><input type=\"checkbox\" value=\""+value+"\" name=\"" + type + "\" "+disabled+" checked=\"checked\"> <span>"+c+"</span></label>")
                 .appendTo( appendTo );
-                /*.tooltip("<p>" + map[c].description + "<br />Father class: " + map[c].augments + ( (map[c].requires)? "<br />Requires: " + map[c].requires : "" ) +"</p>")
-                .position({ offset: "25 10" });*/
         }    
-    },
-    checkIt: function(c) {
-
-        console.log( $("input[value="+c+"]") );
-
-        $("input[value="+c+"]").attr("checked","checked");
-    },
-    
-    unCheckIt: function(c) {
-    
-        if (custom.isShared(c)) { 
-            return;
-        }
-    
-        $("input[value="+c+"]").removeAttr("checked");
-    },
-    
-    isShared: function(c) {
-        
-        var r = false;
-        
-        $("input:checked").each(function(event){
-        
-            var t = map[$(this).val()];
-            
-            if (!t) return;
-            
-            if (t.augments && c == t.augments) {
-                r = true;    
-            }
-                    
-            if (t.depends) {
-                if (t.depends.join(",").indexOf(c) > -1) {
-                    r = true;
-                }
-            }
-    
-        });
-        
-        return r;    
-    },
-    
-    checkMap: function( c ) {
-    
-        if ( !c ) { return; }
-        
-        if (c.type === "interface") {
-
-            $( c.dom ).parent().find("input").each(function(i,e) {
-                custom.action( $(this).val() );
-            });
-
-        } else {
-            
-            if (c.augments) {
-                custom.action( c.augments );
-                custom.checkMap( map[c.augments] );
-            }   
-
-            if (c.requires) {
-                
-                $(c.requires).each(function(i,e){        
-                    custom.action( e );
-                    custom.checkMap( map[e] );
-                });  
-            }
-        }
     }
 };
         
