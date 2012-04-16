a4819aeb2b28098df7bcc66359da95d9a09c17f1
Leandro Linares <lean8086@gmail.com>
2012-04-16 14:55:24 -0300

    Add friendly name to navigation and update that on configuration and event includes for widgets demos. Add the menu-selected classname for fast update of next header

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var sys = require("util"),
 	express = require("express"),
 	Packer = require("../chico/libs/packer/packer").Packer,
 	app = express.createServer(),
+	port = process.argv[2] || 8080,
+	countries = require("./models/countries").countries,
 	meta;
 
 // Configure webserver
@@ -63,6 +65,18 @@ function friendly(string, type) {
 	return a.join(" ");	
 }
 
+function camelCase(string) {
+	
+	// From "auto-complete" to "autoComplete"
+	var a = [];
+	
+	string.split("-").forEach(function (word, i) {
+		a[i] = (i > 0) ? ucfirst(word) : word;
+	});
+	
+	return a.join("");	
+}
+
 function createNavMap(type) {
 
 	// Final result
@@ -94,8 +108,6 @@ meta = {
 	"widgets": createNavMap("widgets"),
 	"guides": createNavMap("guides"),
 	"title": "Chico UI, MercadoLibre's open source web tools.",
-	"label": "test",
-	"instanceLabel": "test"
 };
 
 // Order versions foldernames into an array
@@ -207,9 +219,7 @@ app.post("/download", function (req, res) {
 
 app.get("/suggest", function (req, res) {
 	
-	var countries = ["Afghanistan", "Akrotiri", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Ashmore and Cartier Islands", "Australia", "Austria", "Azerbaijan", "Bahamas, The", "Bahrain", "Bangladesh", "Barbados", "Bassas da India", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Clipperton Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo, Democratic Republic of the", "Congo, Republic of the", "Cook Islands", "Coral Sea Islands", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Dhekelia", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Europa Island", "Falkland Islands (Islas Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "French Southern and Antarctic Lands", "Gabon", "Gambia, The", "Gaza Strip", "Georgia", "Germany", "Ghana", "Gibraltar", "Glorioso Islands", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard Island and McDonald Islands", "Holy See (Vatican City)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Jan Mayen", "Japan", "Jersey", "Jordan", "Juan de Nova Island", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nauru", "Navassa Island", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paracel Islands", "Paraguay", "Peru", "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia and Montenegro", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Spratly Islands", "Sri Lanka", "Sudan", "Suriname", "Svalbard", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tromelin Island", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands", "Wake Island", "Wallis and Futuna", "West Bank", "Western Sahara", "Yemen", "Zambia", "Zimbabwe"],
-	
-		result = [],
+	var result = [],
 		
 		i = countries.length;
 	
@@ -232,6 +242,7 @@ app.get("/suggest", function (req, res) {
 app.get("/", function (req, res) {
 	
 	meta.layout = "layout_1col";
+	meta.selected = "";
 	
 	res.render("index", meta);
 });
@@ -241,15 +252,19 @@ app.get("/:section", function (req, res) {
 	
 	meta.layout = "layout_1col";
 	meta.title = "Chico UI | " + ucfirst(req.params.section);
+	meta.selected = req.params.section;
 	
 	res.render(req.params.section, meta);
 });
 
-// Widgets (L1 + L2)
+// Widgets, Guides, etc. (L1 + L2)
 app.get("/:section/:file", function (req, res) {
 	
 	meta.layout = "layout_2cols";
-	meta.title = "Chico UI | " + ucfirst(req.params.section) + " | " + friendly(req.params.file, req.params.section);
+	meta.friendly = friendly(req.params.file, req.params.section);
+	meta.camelCase = camelCase(req.params.file);
+	meta.title = "Chico UI | " + ucfirst(req.params.section) + " | " + meta.friendly;
+	meta.selected = req.params.section;
 	
 	res.render(req.params.section + "/" + req.params.file, meta);
 });
@@ -260,7 +275,7 @@ app.get("/:section/:file", function (req, res) {
 */
 
 // Initialize application
-app.listen(process.argv[2] || 8080);
+app.listen(port);
 
 // Feedback
-console.log("Server running...");
+console.log("Server running on port " + port);
